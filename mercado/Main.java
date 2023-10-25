3c953dbc5cea47f780ed9ed3c6b97a6d19b1604f
NweoWasTaken <116028942+horaciohenao@users.noreply.github.com>
2023-10-25 00:13:05 -0500

    9

---
+++ mercado/Main.java
@@ -0,0 +1,22 @@
+package codigos.mercado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    List<Integer> codigo = new ArrayList<Integer>();
+    List<Integer> nombre = new ArrayList<Integer>();
+    List<Integer> marca = new ArrayList<Integer>();
+    List<Integer> color = new ArrayList<Integer>();
+    List<Integer> preCom = new ArrayList<Integer>();
+    List<Integer> preVen = new  ArrayList<Integer>();
+    List<Integer> porDesMax = new ArrayList<Integer>();
+    List<Integer> uniExi = new ArrayList<Integer>();
+    List<Integer> metMed = new ArrayList<Integer>();
+    List<Integer> categoria = new ArrayList<Integer>();
+
+    
+
+
+}
